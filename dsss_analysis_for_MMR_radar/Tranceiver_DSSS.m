71d82dc83fa990a0afd79d6b2d5e6850fe6899f7
Pouyan <pouyank13@me.com>
2017-01-12 09:52:32 -0700

    okay so I think everything is right. One big problem. Can't run code bc array size will be in the billions. Will branch and try some simplified methods

--- dsss_analysis_for_MMR_radar/Tranceiver_DSSS.m
+++ dsss_analysis_for_MMR_radar/Tranceiver_DSSS.m
@@ -57,18 +57,6 @@ c_t = (0:c_L-1)*c_T;              % Time vector
 
 c = cos(2*pi*c_fo*c_t);          % First row wave
 c2 = cos(2*pi*c_fo*c_t+pi);
-% c_n = 2^nextpow2(c_L);
-% c_spectrum = fft(c,c_n);
-% c_spectrum_mag = abs(c_spectrum/c_n);
-% c_spectrum_s = c_spectrum_mag(:,1:c_n/2+1);
-% c_spectrum_s(:,2:end-1) = 2*c_spectrum_s(:,2:end-1);
-% plot(0:(c_Fs/c_n):(c_Fs/2-c_Fs/c_n),c_spectrum_s(1:c_n/2))
-
-% Scale PBRS array appropriately
-% each sample bit gets expanded into 158 bits
-%PRBS_new(1:1:159) = PRBS(1,1);
-%PRBS_new(160:1:318) = PRBS(1,2);
-%PRBS_new(319:1:477) = PRBS(1,3); etc
 
 for k = 1:N;
     if k == 1
@@ -82,34 +70,16 @@ end
 dsss_sig = [];
 
 for k=1:c_L
-    if PRBS(1,k) == 0
+    if PRBS_new(1,k) == 0
         dsss_sig = [dsss_sig c];
     else
         dsss_sig = [dsss_sig c2];
     end
 end
 %%
-
 dsss_n = 2^nextpow2(size(dsss_sig));
 dsss_spectrum = fft(dsss_sig,dsss_n);
 dsss_spectrum_mag = abs(dsss_spectrum/dsss_n);
 dsss_spectrum_s = dsss_spectrum_mag(:,1:dsss_n/2+1);
 dsss_spectrum_s(:,2:end-1) = 2*dsss_spectrum_s(:,2:end-1);
 plot(0:(c_Fs/dsss_n):(c_Fs/2-c_Fs/dsss_n),dsss_spectrum_s(1:dsss_n/2))
-
-% dsss_size = size(dsss_sig, 2);
-% NFFT = 2^nextpow2(dsss_size); % Next power of 2 from length of y
-% DSSS_spectrum = fft(dsss_sig,NFFT);
-% f = 81.5e9*linspace(0,1,NFFT/2+1);
-% DSSS_P2 = abs(DSSS_spectrum/NFFT);
-% DSSS_P1 = DSSS_P2(:,1:NFFT/2+1);
-% DSSS_P1(:,2:end-1) = 2*DSSS_P1(:,2:end-1);
-% plot(f,DSSS_P1)
-
-
-%figure,plot([1:12120],dsss_sig);
-%axis([-1 12220 -1.5 1.5]);
-%title('\bf\it DSSS Signal');
-
-% Plotting the FFT of DSSS signal
-%figure,plot([1:12120],abs(fft(dsss_sig)))
