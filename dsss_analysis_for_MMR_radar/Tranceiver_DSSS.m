5462af25f782a93c5e9116875607e35aaade7bc6
Pouyan <pouyank13@me.com>
2017-01-12 12:41:13 -0700

    reduced N to 12 and it appears to be working

--- dsss_analysis_for_MMR_radar/Tranceiver_DSSS.m
+++ dsss_analysis_for_MMR_radar/Tranceiver_DSSS.m
@@ -46,29 +46,33 @@ plot(f,P1)
 clear all
 close all
 clc
-N = 1024;
+% PN Code Parameters
+N = 8;
 PRBS = round(rand(1,N));
-
+fc = 1.5e9;
+Tp = N/fc;
+% Carrier Parameters
 c_fo = 79e9;
-c_Fs = 3*c_fo;                    % Sampling frequency
+c_Fs = 2.1*c_fo;                  % Sampling frequency
 c_T = 1/c_Fs;                     % Sampling period
-c_L = 161792;                     % Length of signal in samples
+c_L = c_Fs*Tp;                    % Length of signal in samples
 c_t = (0:c_L-1)*c_T;              % Time vector
-
-c = cos(2*pi*c_fo*c_t);          % First row wave
+% define a scaling factor to expand array size of DSSS code to carrier size
+sf = c_L/N;
+% Define 
+c = cos(2*pi*c_fo*c_t);           
 c2 = cos(2*pi*c_fo*c_t+pi);
-
+%% Scale DSSS array appropriately
 for k = 1:N;
     if k == 1
-        PRBS_new(1:1:158) = PRBS(1,1);
+        PRBS_new(1:1:round(sf)) = PRBS(1,1);
     else
-        PRBS_new((k-1)*158+1:1:k*158) = PRBS(1,k);
+        PRBS_new((k-1)*round(sf)+1:1:k*round(sf)) = PRBS(1,k);
     end
 end
 
-%%
+%% Mix the signals to create DSSS signal
 dsss_sig = [];
-
 for k=1:c_L
     if PRBS_new(1,k) == 0
         dsss_sig = [dsss_sig c];
@@ -77,9 +81,12 @@ for k=1:c_L
     end
 end
 %%
-dsss_n = 2^nextpow2(size(dsss_sig));
+dsss_n = 2^nextpow2(size(dsss_sig,2));
 dsss_spectrum = fft(dsss_sig,dsss_n);
 dsss_spectrum_mag = abs(dsss_spectrum/dsss_n);
 dsss_spectrum_s = dsss_spectrum_mag(:,1:dsss_n/2+1);
 dsss_spectrum_s(:,2:end-1) = 2*dsss_spectrum_s(:,2:end-1);
 plot(0:(c_Fs/dsss_n):(c_Fs/2-c_Fs/dsss_n),dsss_spectrum_s(1:dsss_n/2))
+
+
+
